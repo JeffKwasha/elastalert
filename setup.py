ed513c6dcb4d8b2a6b530f6834b59755aacc7e51
nsano-rururu <nsano@ae.em-net.ne.jp>
2021-01-10 18:15:28 +0900

    Remove prometheus metrics

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'mock>=2.0.0',
         'prison>=0.1.2',
         'PyStaticConfiguration>=0.10.3',
-        'prometheus_client>=0.6.0',
         'python-dateutil>=2.6.0,<2.7.0',
         'PyYAML>=5.1',
         'requests>=2.10.0',
